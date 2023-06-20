92c83101ff4fe2cd5938579feb7321e60ce78614
Gian <gianmendoza64@gmail.com>
2023-06-20 09:39:03 +0200

    Update app.js
    apre e chiude il popup

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-function OpenPopUp()
+const name=document.querySelector("open")
+function OpenPopUp(pop)
 {
     console.log("run");
-      document.getElementById('pop').style.display="block";
+{
+    document.getElementById('pop').style.display="block";
+}
+}
+
+document.getElementById("close").onclick=function(e)
+{
+    document.getElementById('pop').style.display="none";
 }

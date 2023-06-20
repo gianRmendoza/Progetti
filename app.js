5922038802d5fb6eb21f5c7fbe24e6650c5bf94a
Gian <gianmendoza64@gmail.com>
2023-06-20 17:00:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const name=document.querySelector("open")
-function OpenPopUp(pop)
-{
-    console.log("run");
+for(var contatore=0;)
+var messages = [];
+
+
+
+function openPopUp(pop)
 {
     document.getElementById('pop').style.display="block";
-}
 }
 
 document.getElementById("close").onclick=function(e)
 {
     document.getElementById('pop').style.display="none";
+}
+function sendMessage()
+{
+    
+    var testo = document.getElementById("Testo").value;
+    console.log(messages); 
 }

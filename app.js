79b30f00f062854cb7d15d06e52172cca4663fb8
Gian <gianmendoza64@gmail.com>
2023-06-21 15:46:29 +0200

    Update app.js
    aggiunto funzioni per i messaggi

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
-for(var contatore=0;)
-var messages = [];
-
-
-
 function openPopUp(pop)
 {
     document.getElementById('pop').style.display="block";
 }
 
-document.getElementById("close").onclick=function(e)
+document.getElementById("close").onclick = function(e)
 {
     document.getElementById('pop').style.display="none";
 }
-function sendMessage()
-{
-    
+function sendMessage(){
+    // Inserisco il testo dentro una variabile
     var testo = document.getElementById("Testo").value;
-    console.log(messages); 
+
+    // Creo un elemento html, aggiungo dentro il testo, aggiungo le classi e stampo l'elemnto dentro il contenitore
+    var newElement = document.createElement('div');
+    newElement.innerText = testo;
+    newElement.classList.add("message", "messageFrom");
+    document.getElementById("chatContent").appendChild(newElement); //inserisce il "figlio"(newElement) all'interno della "madre"(chat)
+
+    // svuoto il campo di testo
+    document.getElementById("Testo").value="";
+
+    //Chiama una funzione che restituisce un messaggio di attesa
+    loadingMessage()
+    //Chiamare una nuova funzione da creare che restituisce un messaggio che andrà in messageTo
+    setTimeout(
+        function() {
+            console.log("run");
+            receiveMessage();
+           }, 2000);
+    
+}
+
+function receiveMessage()
+{
+    var newElement2 = document.createElement('div');
+    newElement2.innerText = "Ciao!";
+    newElement2.classList.add("message", "messageTo");
+    document.getElementById("chatContent").appendChild(newElement2);
+}
+function loadingMessage()
+{
+    var newElement3 = document.createElement('div');
+    newElement3.innerText = "...";
+    newElement3.classList.add("message", "messageLoading");
+    document.getElementById("chatContent").appendChild(newElement3);
 }

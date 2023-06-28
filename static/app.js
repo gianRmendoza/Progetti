649319b88b722ff0a54dae65e2015b18e4af8ae1
Gian <gianmendoza64@gmail.com>
2023-06-28 12:06:55 +0200

    Create app.js

---
+++ static/app.js
@@ -0,0 +1,69 @@
+function openPopUp(pop)
+{
+    document.getElementById('pop').style.display="block";
+    document.getElementById("Testo").focus();
+}
+
+document.getElementById("close").onclick = function(e)
+{
+    document.getElementById('pop').style.display="none";
+}
+
+function sendMessage(){
+    document.getElementById("Testo").focus();
+    // Inserisco il testo dentro una variabile
+    var testo = document.getElementById("Testo").value;
+
+    // Creo un elemento html, aggiungo dentro il testo, aggiungo le classi e stampo l'elemnto dentro il contenitore
+    var newElement = document.createElement('div');
+    newElement.innerText = testo;
+    newElement.classList.add("message", "messageFrom");
+    document.getElementById("chatContent").appendChild(newElement); //inserisce il "figlio"(newElement) all'interno della "madre"(chat)
+
+    // svuoto il campo di testo
+    document.getElementById("Testo").value="";
+
+    //Chiama una funzione che restituisce un messaggio di attesa
+    setTimeout(
+        function() {
+            console.log("run");
+            loadingMessage();
+           }, 500);
+    //Chiamare una nuova funzione da creare che restituisce un messaggio dopo 2 secondi che andrà in messageTo
+    setTimeout(
+        function() {
+            console.log("run");
+            receiveMessage();
+            document.getElementById("loading").remove();
+           }, 2000);
+           scrollToEnd()
+}
+
+function receiveMessage()
+{
+    var newElement2 = document.createElement('div');
+    newElement2.innerText = "Ciao!";
+    newElement2.classList.add("message", "messageTo");
+    document.getElementById("chatContent").appendChild(newElement2);
+    scrollToEnd()
+}
+function loadingMessage()
+{
+    var newElement3 = document.createElement('div');
+    newElement3.innerText = "...";
+    newElement3.classList.add("message", "messageLoading");
+    newElement3.setAttribute("id","loading");
+    document.getElementById("chatContent").appendChild(newElement3);
+    scrollToEnd()
+}
+function scrollToEnd(){
+	var chatList = document.getElementById("chatContent");
+	chatList.scrollTop = chatList.scrollHeight;
+}
+
+document.getElementById("Testo")
+    .addEventListener("keyup", function(e) {
+        if (e.keyCode === 13) {
+            document.getElementById("submit").click();
+        }
+    });

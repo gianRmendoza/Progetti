ac7087b997302319b3a1a07c6f334d227e230de8
Gian <gianmendoza64@gmail.com>
2023-06-30 14:57:06 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 static_dir = str(os.path.abspath(os.path.join(__file__, "..", "static")))
 
@@ -16,7 +16,8 @@ print(static_dir)
 def index():
     return render_template('chatbot.html')
 
-
+#controlla la question col try catch e chiama l'istruzione chatbot_answer_question(question)
+#per restituire la risposta
 @app.route('/answer-question', methods=['POST'])
 def answer_question():
     try:
@@ -27,16 +28,22 @@ def answer_question():
     if "question" not in req_data.keys():
         return jsonify({'error': "No question provided"}), 400
 
-    question = request["question"]
+    question = req_data["question"]
 
     if type(question) is not str:
         return jsonify({'error': "The provided question is not a String"}), 400
 
+    if  question == "":
+        return jsonify({'error': "The provided question has no value"}), 400
+
     answer = chatbot_answer_question(question)
     return jsonify({'answer': answer}), 200
 
+#restituisce la risposta alla domanda    
 def chatbot_answer_question(question: str) -> str:
-    return "fake answer"
+    data = question
+    result = 'Hi ' + data + 'How can I help you?'
+    return result
 
 if __name__ == '__main__':
     app.run(port=4242, debug=True)  
